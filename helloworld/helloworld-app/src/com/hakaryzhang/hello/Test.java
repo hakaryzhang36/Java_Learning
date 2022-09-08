731b6d9800893dd767f25040bbdabfa5b491dc88
hakaryzhang36 <hakaryzhang36@163.com>
2022-09-08 20:02:04 +0800

    try more

---
+++ helloworld/helloworld-app/src/com/hakaryzhang/hello/Test.java
@@ -0,0 +1,10 @@
+package com.hakaryzhang.hello;
+
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("访问了 Test Main");
+    }
+
+    public Test() {
+    }
+}
